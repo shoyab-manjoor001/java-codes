a1558da410edb4a002171dc5d058280084a46a42
Shoyab <s.manjoor001@gmail.com>
2024-02-24 17:01:19 +0530

    added sleep and priority in thread

--- JavaCodes/ThreadEx/Demo.java
+++ JavaCodes/ThreadEx/Demo.java
@@ -4,6 +4,11 @@ class A extends Thread {
     public void run() {
         for (int j = 1; j <= 100; j++) {
             System.out.println("In A show..");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -13,6 +18,11 @@ class B extends Thread {
     public void run() {
         for (int j = 1; j <= 100; j++) {
             System.out.println("In B show..");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
     }
@@ -23,7 +33,14 @@ public class Demo {
     public static void main(String[] args) {
         A ob1 = new A();
         B ob2 = new B();
+
+        ob1.setPriority(Thread.MAX_PRIORITY);
         ob1.start();
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         ob2.start();
     }
 }
