90df16f21ed19632f1c69dec9a30e54b4024a34b
Shoyab <s.manjoor001@gmail.com>
2024-03-02 22:40:36 +0530

    Added arraylist example 2

---
+++ JavaCodes/CollectionFrameWork/ALDemo2.java
@@ -0,0 +1,18 @@
+package CollectionFrameWork;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.RandomAccess;
+
+public class ALDemo2 {
+
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList<>();
+        LinkedList<String> ll = new LinkedList<String>();
+        System.out.println(al instanceof Serializable);
+        System.out.println(ll instanceof Cloneable);
+        System.out.println(al instanceof RandomAccess);
+        System.out.println(ll instanceof RandomAccess);
+    }
+}
