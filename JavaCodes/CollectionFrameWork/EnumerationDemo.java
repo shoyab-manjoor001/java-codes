2c1196f386f3fd0f295b68315f4093142120e73c
Shoyab <s.manjoor001@gmail.com>
2024-03-03 19:40:14 +0530

    Added Enumeration Example

---
+++ JavaCodes/CollectionFrameWork/EnumerationDemo.java
@@ -0,0 +1,24 @@
+package CollectionFrameWork;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class EnumerationDemo {
+
+    public static void main(String[] args) {
+
+        Vector v = new Vector<>();
+        for (int i = 1; i <= v.capacity(); i++) {
+            v.addElement(i);
+        }
+        System.out.println("Original Vector: " + v);
+        Enumeration e = v.elements();
+        while (e.hasMoreElements()) {
+            Integer i = (Integer) e.nextElement();
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+        System.out.println(v);
+    }
+}
